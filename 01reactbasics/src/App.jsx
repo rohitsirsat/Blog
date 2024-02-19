c2cc7a816fbdbea80a8c55d1e8cdb453431e307a
rohit <rohitsirsat855@gmail.com>
2024-02-19 19:08:30 +0530

    delete practice files

--- 01reactbasics/src/App.jsx
+++
@@ -1,7 +0,0 @@
-import "./App.css";
-
-function App() {
-  return <h1>chai aur react with vite</h1>;
-}
-
-export default App;
--- 10todoContext/src/context/index.js
+++
@@ -1,1 +0,0 @@
-import { useTodo, TodoProvider, TodoContext } from "./TodoContext.js";
